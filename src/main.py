9dba91fe8929803c131c6c4371e4ce3798fa8d92
Unai <unailaconcha@gmail.com>
2024-11-26 16:37:45 +0100

    Detección cartas mejorado

--- src/main.py
+++ src/main.py
@@ -5,18 +5,16 @@ from roiExtractor import ROIExtractor
 import os
 
 # Cargar los valores desde el archivo npz
-with np.load('./static/npz/calibration_data.npz') as data:
-    loaded_mtx = data['camera_matrix']
-    loaded_dist = data['dist_coeffs']
+current_dir = os.path.dirname(os.path.abspath(__file__))
+calibration_path = os.path.join(current_dir, '../static/npz/calibration_data.npz')
 
-# Función para obtener la línea que pasa por dos puntos
-def calculate_line(p1, p2):
-    m = (p2[1] - p1[1]) / (p2[0] - p1[0]) if p2[0] != p1[0] else None
-    if m is not None:
-        b = p1[1] - m * p1[0]
-    else:
-        b = p1[0]  # Vertical line case
-    return m, b
+# Verificar si el archivo existe antes de intentar cargarlo
+if os.path.exists(calibration_path):
+    with np.load(calibration_path) as data:
+        loaded_mtx = data['camera_matrix']
+        loaded_dist = data['dist_coeffs']
+else:
+    raise FileNotFoundError(f"El archivo de calibración no se encuentra en la ruta especificada: {calibration_path}")
 
 # Función para desplazar dos puntos una distancia N de forma paralela
 def parallel_shift(p1, p2, distance):
@@ -24,6 +22,30 @@ def parallel_shift(p1, p2, distance):
     unit_direction = direction / np.linalg.norm(direction)
     shift_vector = unit_direction * distance
     return p1 + shift_vector, p2 + shift_vector
+
+def ordenar_puntos(box):
+    # Convertimos a un array numpy con 4 filas y 2 columnas
+    puntos = np.array(box)
+
+    # Calculamos el centroide (promedio de las coordenadas X y Y)
+    centroide = np.mean(puntos, axis=0)
+
+    # Clasificamos los puntos con respecto al centroide (orden en el sentido de las agujas del reloj)
+    puntos_ordenados = sorted(puntos, key=lambda p: (np.arctan2(p[1] - centroide[1], p[0] - centroide[0])))
+
+    # Convertimos a numpy array en el orden adecuado
+    puntos_ordenados = np.array(puntos_ordenados, dtype="float32")
+
+    # Identificar cuál es el lado largo y cuál es el corto
+    d1 = np.linalg.norm(puntos_ordenados[0] - puntos_ordenados[1])  # Distancia entre punto 0 y 1
+    d2 = np.linalg.norm(puntos_ordenados[1] - puntos_ordenados[2])  # Distancia entre punto 1 y 2
+
+    if d1 > d2:
+        # Si el lado entre los puntos 0 y 1 es el más largo, debemos reorganizar los puntos
+        # para asegurarnos de que el lado largo siempre esté representado como la "altura"
+        puntos_ordenados = np.roll(puntos_ordenados, -1, axis=0)  # Rotar los puntos en sentido antihorario
+
+    return puntos_ordenados
 
 # Función para extraer los puntos en los que intersectan dos líneas
 def find_intersection(m1, b1, m2, b2):
@@ -116,8 +138,9 @@ while success and cv2.waitKey(1) == -1:
 
     # Cerrar ventanas de contornos que ya no están presentes
     for contour_id in existing_contour_ids - current_contour_ids:
-        cv2.destroyWindow(active_windows[contour_id])
-        del active_windows[contour_id]
+        if contour_id in active_windows:
+            cv2.destroyWindow(active_windows[contour_id])
+            del active_windows[contour_id]
     
     # Dibujar y mostrar la región de interés de cada carta detectada
     for idx, c in enumerate(contours):
@@ -131,75 +154,124 @@ while success and cv2.waitKey(1) == -1:
         # Normalizar las coordenadas a enteros
         box = np.int32(box)
 
-        # dibujar contornos
-        cv2.drawContours(thresh, [box], 0, (0,0, 255), 3)
-        cv2.drawContours(undistorted_frame, [box], 0, (0,0, 255), 3)
+        # Utilizar esta función para ordenar los puntos antes de definir `src_pts`
+        box = ordenar_puntos(box)
+
+        # Dibujar contornos
+        #cv2.drawContours(thresh, [box], 0, (0,0, 255), 3)
+        #cv2.drawContours(undistorted_frame, [box], 0, (0,0, 255), 3)
 
         # Obtener dimensiones reales del rectángulo rotado
         width = int(rect[1][0])  # Ancho del rectángulo
         height = int(rect[1][1])  # Alto del rectángulo
 
-        # Validar que las dimensiones sean válidas
-        if width > 0 and height > 0:
+        # Asegurarse de que height sea el lado mayor y width el lado menor
+        if height < width:
+            width, height = height, width
             
-            # Crear los puntos del rectángulo detectado
-            src_pts = np.array(box, dtype="float32")
-            
-            # Definir puntos destino con el tamaño exacto del rectángulo
-            dst_pts = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype="float32")
-            
-            # Calcular la transformación de perspectiva
-            M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-            
-            # Aplicar la transformación a la imagen original
-            warped = cv2.warpPerspective(undistorted_frame, M, (width, height))
+        # Definir puntos destino con el tamaño exacto del rectángulo
+        dst_pts = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype="float32")
 
-            # Escalar la ventana para que se ajuste mejor al tamaño original
-            escala = 1.5  # Factor de escala para aumentar el tamaño de la ventana
-            scaled_width = int(width * escala)
-            scaled_height = int(height * escala)
-            warped_resized = cv2.resize(warped, (scaled_width, scaled_height), interpolation=cv2.INTER_LINEAR)
-            
-            # Aplicar un threshold al área recortada (con fondo negro)
-            gray_card = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)  # Convertir la carta a escala de grises
-            _, card_thresh = cv2.threshold(gray_card, 127, 255, cv2.THRESH_BINARY)  # Aplicar threshold binario
 
-            # Mantener el fondo negro
-            warped[np.where(card_thresh == 0)] = [0, 0, 0]
+        # Crear los puntos del rectángulo detectado
+        src_pts = np.array(box, dtype="float32")
+        
+        # Calcular la transformación de perspectiva
+        M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+        
+        # Aplicar la transformación a la imagen original
+        warped = cv2.warpPerspective(undistorted_frame, M, (width, height))
 
-            # Detectar si la carta es roja o negra
-            # Convertir la carta a espacio de color HSV
-            hsv_card = cv2.cvtColor(warped, cv2.COLOR_BGR2HSV)
+        # Rotar la imagen resultante si es necesario para que siempre esté en orientación vertical
+        if warped.shape[1] > warped.shape[0]:  # Si el ancho es mayor que la altura, significa que está horizontal
+            warped = cv2.rotate(warped, cv2.ROTATE_90_CLOCKWISE)
 
-            # Definir los límites del color rojo en HSV
-            lower_red1 = np.array([0, 50, 50])
-            upper_red1 = np.array([10, 255, 255])
-            lower_red2 = np.array([170, 50, 50])
-            upper_red2 = np.array([180, 255, 255])
+        # Escalar la ventana para que se ajuste mejor al tamaño original
+        escala = 1.5  # Factor de escala para aumentar el tamaño de la ventana
+        scaled_width = int(width * escala)
+        scaled_height = int(height * escala)
+        warped_resized = cv2.resize(warped, (scaled_width, scaled_height), interpolation=cv2.INTER_LINEAR)
+        
+        # Aplicar un threshold al área recortada (con fondo negro)
+        #gray_card = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)  # Convertir la carta a escala de grises
+        #_, card_thresh = cv2.threshold(gray_card, 127, 255, cv2.THRESH_BINARY)  # Aplicar threshold binario
 
-            # Crear máscara para detectar rojos
-            mask_red1 = cv2.inRange(hsv_card, lower_red1, upper_red1)
-            mask_red2 = cv2.inRange(hsv_card, lower_red2, upper_red2)
-            mask_red = mask_red1 + mask_red2
+        # Mantener el fondo negro
+        #warped[np.where(card_thresh == 0)] = [0, 0, 0]
 
-            # Calcular la cantidad de píxeles rojos
-            red_pixels = np.sum(mask_red > 0)
+        # Definir una sub-región central de la carta para evitar los bordes
+        h, w = warped.shape[:2]
+        offset = 0  # Porcentaje para reducir el ROI a un 60% del área original
+        x_start = int(w * offset)
+        y_start = int(h * offset)
+        x_end = int(w * (1 - offset))
+        y_end = int(h * (1 - offset))
+        warped_central = warped[y_start:y_end, x_start:x_end]
 
-            # Calcular la cantidad de píxeles negros (o áreas oscuras)
-            black_pixels = np.sum(gray_card < 50)
+        # Convertir la sub-región de la carta a espacio de color HSV
+        hsv_card = cv2.cvtColor(warped_central, cv2.COLOR_BGR2HSV)
+        
+        # Detectar si la carta es roja o negra
+        # Convertir la carta a espacio de color HSV
+        hsv_card = cv2.cvtColor(warped, cv2.COLOR_BGR2HSV)
 
-            # Determinar si la carta es Roja o Negra
-            color_label = "Roja" if red_pixels > black_pixels else "Negra"
-            
-            # Añadir la etiqueta de color sobre la carta detectada
-            cv2.putText(warped_resized, color_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            
-            # Mostrar la región recortada en una ventana con tamaño ajustado
-            window_name = f'Carta_Rotada_{idx}'
-            cv2.imshow(window_name, warped_resized)
-            
-            # Mover las ventanas de las cartas a la parte inferior derecha
-            cv2.moveWindow(window_name, 650 + (idx % 5) * (scaled_width + 10), 500 + (idx // 5) * (scaled_height + 10))
+        # Definir los límites del color rojo en HSV
+        lower_red1 = np.array([0, 50, 50])
+        upper_red1 = np.array([10, 255, 255])
+        lower_red2 = np.array([170, 50, 50])
+        upper_red2 = np.array([180, 255, 255])
+        # Definir los límites del color amarillo en HSV
+        lower_yellow = np.array([20, 100, 100])
+        upper_yellow = np.array([30, 255, 255])
+
+        # Crear máscara para detectar rojos
+        mask_red1 = cv2.inRange(hsv_card, lower_red1, upper_red1)
+        mask_red2 = cv2.inRange(hsv_card, lower_red2, upper_red2)
+        mask_red = mask_red1 + mask_red2
+        mask_yellow = cv2.inRange(hsv_card, lower_yellow, upper_yellow)
+
+        # Calcular la cantidad de píxeles rojos
+        red_pixels = np.sum(mask_red > 0)
+
+        # Calcular la cantidad de píxeles oscuros (negros) en escala de grises
+        gray_card_central = cv2.cvtColor(warped_central, cv2.COLOR_BGR2GRAY)
+        mask_black = cv2.inRange(gray_card_central, 0, 80)
+        #black_pixels = np.sum(gray_card_central < 80)
+
+        # Calcular la cantidad de píxeles para cada color
+        total_pixels = hsv_card.shape[0] * hsv_card.shape[1]
+        red_pixels = np.sum(mask_red > 0)
+        yellow_pixels = np.sum(mask_yellow > 0)
+        black_pixels = np.sum(mask_black > 0)
+
+        # Calcular el porcentaje de cada color
+        red_percentage = (red_pixels / total_pixels) * 100
+        yellow_percentage = (yellow_pixels / total_pixels) * 100
+        black_percentage = (black_pixels / total_pixels) * 100
+
+        # Determinar el color predominante
+        color_label = "Negra"
+        if red_percentage > yellow_percentage and red_percentage > black_percentage:
+            color_label = "Roja"
+        elif yellow_percentage > red_percentage and yellow_percentage > black_percentage:
+            color_label = "Amarilla"
+
+
+        # Añadir la etiqueta de porcentajes y el color sobre la carta detectada
+        cv2.putText(warped_resized, f"Color: {color_label}", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(warped_resized, f"Roja: {red_percentage:.2f}%", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(warped_resized, f"Amarilla: {yellow_percentage:.2f}%", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(warped_resized, f"Negra: {black_percentage:.2f}%", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+        
+        # Mostrar la región recortada en una ventana con tamaño ajustado
+        window_name = f'Carta_Rotada_{idx}'
+        cv2.imshow(window_name, warped_resized)
+
+        # Actualizar el diccionario de ventanas activas
+        active_windows[idx] = window_name
+        
+        # Mover las ventanas de las cartas a la parte inferior derecha
+        cv2.moveWindow(window_name, 650 + (idx % 5) * (scaled_width + 10), 500 + (idx // 5) * (scaled_height + 10))
 
         
         # Extraer la región del box y mostrarla en una ventana separada
